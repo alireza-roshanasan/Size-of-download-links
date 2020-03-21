1271cea589586786c114495ce8505bc30969944c
alireza roshanasan <alireza.roshanasan@gmail.com>
2020-03-21 18:31:40 +0430

    update

--- list_size.py
+++ list_size.py
@@ -1,34 +1,68 @@
-import urllib
 import argparse
-size = 0
-comp = 0
-ucomp = 0
+import sys
+if sys.version_info[0] == 3:
+    from urllib.request import urlopen
+    from urllib.error import URLError as error
+else:
+    from urllib import urlopen
+    error = IOError
+
+size, comp, ucomp = 0, 0, 0
+
+
+class Printer():
+    def __init__(self, *args):
+        data = ""
+        for i in args:
+            data += str(i)
+            data += ""
+        sys.stdout.write("\r\x1b[K"+data.__str__())
+        sys.stdout.flush()
+
+
 def to_gb(x):
-	return int(x) / 1024 / 1024 / 1024
+    return int(x) / 1024 / 1024 / 1024
+
+
 def to_mb(x):
-	return int(x) / 1024 / 1024 
-def check(loc):
-	global size, comp, ucomp
-	f = open(loc,'r').readlines()
-	for i in f:
-		try:
-			o = urllib.urlopen(i.replace('\n',''))
-			size = size + int(o.info()['Content-Length'])
-			print i.replace('\n','')+ "  " + str(to_mb(o.info()['Content-Length']))
-			comp = comp + 1
-		except Exception as e:
-			print "ERROR"
-			print i
-			print e
-			ucomp = ucomp + 1
-			pass
-	print "\n"
-	print "################################"
-	print "fullsize(Gb) = " + str(to_gb(size))
-	print "fullsize(Mb) = " + str(to_mb(size)) 
-	print "succes = " + str(comp)
-	print "fail = " + str(ucomp)
-parser = argparse.ArgumentParser()
-parser.add_argument("loc", help="find command")
-args = parser.parse_args()
-check(str(args.loc))
+    return int(x) / 1024 / 1024
+
+
+def get_size(link):
+    link = link.replace("\n", "")
+    try:
+        site = urlopen(link)
+        meta = site.info()
+        if sys.version_info[0] == 3:
+            return int(dict(meta.items()).get("Content-Length"))
+        else:
+            return int(dict(meta.items()).get("content-length"))
+    except error:
+        return False
+    except TypeError:
+        return False
+
+
+location = sys.argv[1]
+with open(location) as urls:
+    size = 0
+    urls = urls.readlines()
+    for url in urls:
+        Printer(
+            "Check ", comp +
+            ucomp, " of ", len(
+                urls), " ---- size until now: ", int(to_mb(size)), " Mb"
+        )
+        if url == "\n":
+            continue
+        size += get_size(url)
+        if size:
+            comp += 1
+        else:
+            ucomp += 1
+print("\n"*2)
+print("count of links:", len(urls))
+print("Succesful:", comp)
+print("Unsuccessful:", ucomp)
+print("Mb:", to_mb(size))
+print("Gb:", to_gb(size))
